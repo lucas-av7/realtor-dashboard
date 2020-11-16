9753d7cfa4fb16c4da8314c9ef9e8b8463381096
Lucas-AV7 <lucas.av7@yahoo.com>
2020-11-16 16:55:41 -0300

    [MUAUn7sE] Connect Flask to MySql DB

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_mysqldb import MySQL
 import os
 
 app = Flask(__name__)
+
+# Config MySql
+app.config['MYSQL_HOST'] = os.environ.get("MYSQL_HOST")
+app.config['MYSQL_USER'] = os.environ.get("MYSQL_USER")
+app.config['MYSQL_PASSWORD'] = os.environ.get("MYSQL_PASSWORD")
+app.config['MYSQL_DB'] = os.environ.get("MYSQL_DB")
+app.config['MYSQL_UNIX_SOCKET'] = os.environ.get("MYSQL_UNIX_SOCKET")
+app.config['MYSQL_CURSORCLASS'] = os.environ.get("MYSQL_CURSORCLASS")
+
+# Init MySQL
+mysql = MySQL(app)
 
 @app.route('/')
 def home():
