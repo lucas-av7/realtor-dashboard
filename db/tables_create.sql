ad0f1469d31fb48d3a634f3ee7be1a3810cc3e43
Lucas-AV7 <lucas.av7@yahoo.com>
2020-11-16 16:35:30 -0300

    [XQZ5aEK9] Create MySQL Environment

---
+++ db/tables_create.sql
@@ -0,0 +1,37 @@
+-- Users
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users` (
+  `id` INTEGER PRIMARY KEY,
+  `email` VARCHAR(100) NOT NULL,
+  `name` VARCHAR(100) NOT NULL,
+  `password` VARCHAR(100) NOT NULL,
+  `register_date` TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+-- products
+DROP TABLE IF EXISTS `products`;
+CREATE TABLE IF NOT EXISTS `products` (
+  `id` INTEGER PRIMARY KEY,
+  `category_id` VARCHAR(100) NOT NULL,
+  `description` TEXT NOT NULL,
+  `img` INTEGER NOT NULL,
+  `register_date` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  `title` VARCHAR(100) NOT NULL
+);
+
+-- photos
+DROP TABLE IF EXISTS `photos`;
+CREATE TABLE IF NOT EXISTS `photos` (
+  `id` INTEGER PRIMARY KEY,
+  `product_id` INTEGER,
+  `register_date` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  `url` VARCHAR(200) NOT NULL
+);
+
+-- Categories
+DROP TABLE IF EXISTS `categories`;
+CREATE TABLE IF NOT EXISTS `categories` (
+  `id` INTEGER PRIMARY KEY,
+  `register_date` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  `title` VARCHAR(100) NOT NULL
+);
